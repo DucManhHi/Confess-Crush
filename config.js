d58dc2062935fcf97e182804643416d9bde5f0f0
DucManhHi <92265877+DucManhHi@users.noreply.github.com>
2021-10-10 23:32:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "demo",
+    introTitle: 'Trịnh Thị Diệu LY',
+    introDesc: `Tình mặn nồng 
+    Yêu đậm sâu 
+    Có đứt tơ hồng 
+    Cũng không làm cho ta xa cách nhau`,
+    btnIntro: '^^nhấn vào đây^^',
+    title: 'Anh chỉ muốn nói 1 điều rằng em là cô gái của đời anh 🥰',
+    desc: 'Anh Yêu Em ',
+    btnYes: 'Yêu Anh <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Em Có Yêu Anh Không <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Em Yêu Anh <333',
+    mess: 'Anh Biết Mà Hihi',
+    messDesc: 'Cứ mãi như thế này Em nhé
+    btnAccept: 'Yêu Em <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
